74cd06033838661851ba5c9470623b6b061b2d6c
Jaden Hawkes <jadenhawkes@gmail.com>
2023-02-22 15:35:12 -0700

    Houses Sorta draw to screen

---
+++ src/components/HouseCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="HouseCard card">
+    <!-- <img :src="house.imgUrl" :alt="house.year"> -->
+<div>
+  <p>{{ house.description }}</p>
+</div>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    house: { type: Object, required: true}
+  },
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="housesPage">
-    <h1>This is the houses page</h1>
+    <!-- <h1>This is the houses page</h1> -->
     <div class="container-fluid">
       <div class="row">
-        <div class="col-12">
-          <HouseCard/>
+        <div class="col-12" v-for="h in houses">
+          <HouseCard :house="h"/>
         </div>
       </div>
     </div>
@@ -17,6 +17,7 @@ import { onMounted, computed } from "vue";
 import { logger } from "../utils/Logger.js";
 import Pop from "../utils/Pop.js";
 import { housesService } from "../services/HousesService.js"
+import { AppState } from "../AppState.js";
 
 export default {
   setup(){
